25c1f193acfca70a06b7c2f3c0e5b98f8eec0f4a
Narcis En <narcis.enescu@outlook.com>
2022-11-27 12:31:51 +0200

    2nd commit. RG

--- main.tf
+++ main.tf
@@ -1,4 +1,19 @@
 terraform {
-  features{}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+    }
+  }
 }
 
+provider "azurerm" {
+  features {}
+}
+    
+resource "azurerm_resource_group" "mtc-rg" {
+  name     = "mtc-resources"
+  location = "East Us"
+  tags = {
+    environment = "dev"
+  }
+}
